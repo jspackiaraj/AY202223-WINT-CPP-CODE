0984518a4071fab7d457e558f809067ea05d50fd
jspackiaraj <90088477+jspackiaraj@users.noreply.github.com>
2023-01-04 08:37:09 +0530

    Update 002_FileWriting.cpp

--- WEEK04-CODE/01_WritingToFile/002_FileWriting.cpp
+++ WEEK04-CODE/01_WritingToFile/002_FileWriting.cpp
@@ -1,18 +1,17 @@
-// writing a text file
+// writing on a text file
 #include <iostream>
 #include <fstream>
 using namespace std;
 
-int main() {
-    cout << "The file will be written in the same directory where the executable is " << endl;
-    ofstream myfile("example.txt");
-    if (myfile.is_open())
-    {
-        myfile << "Hello World.\n";
-        myfile << "On Windows machines, the file will be created with line endings LF CR"<< endl;
-        myfile << "For running on Linux machines, the file will have to be saved with LF Line Ending" << endl;
-        myfile.close();
-    }
-    else cout << "Unable to open file";
-    return 0;
+int main () {
+    cout << "Hello " << endl;
+  ofstream outfile ("example.txt");
+  if (outfile.is_open())
+  {
+    outfile << "This is a line.\n";
+    outfile << "This is another line.\n";
+    outfile.close();
+  }
+  else cout << "Unable to open file";
+  return 0;
 }
